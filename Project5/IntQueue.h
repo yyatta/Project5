bb958f247f0a21e97066835dd057f660c1fe03c6
yyatta <55525589+yyatta@users.noreply.github.com>
2020-10-19 23:04:30 +0900

    sas

---
+++ Project5/IntQueue.h
@@ -0,0 +1,31 @@
+
+#ifndef ___IntQueue
+#define ___IntQueue
+
+typedef struct {
+    int max;
+    int num;
+    int front;
+    int rear;
+    int* que;
+} IntQueue;
+
+int Initialize(IntQueue* q, int max);
+
+int Enque(IntQueue* q, int x);
+
+int Deque(IntQueue* q, int* x);
+
+int Peek(const IntQueue* q, int* x);
+
+void clear(IntQueue* q);
+
+int Capacity(const IntQueue* q);
+
+int Size(const IntQueue* q);
+
+int IsEmpty(const IntQueue* q);
+
+int IsEmpty(const IntQueue* q);
+
+int IsFull(const IntQueue* q);
---
+++ Project5/IntQueueTest.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include "IntQueue.h"
+
+int main(void)
+{
+    lntQueue que;
+
+    if (Initialize(&que, 64) == 1
+    {
+        puts("큐의 생성에 실패하였습니다.");
+            return 1;
+    }
+    while (1) {
+        int m, x;
+
+            print("현재 데이터 수 : %d / %d \n", Size(&que), Capacity(&que));
+            printf("(1)인큐 (2)디큐 (3)피크 (4)출력 (0)종료 : ");
+            scanf_s("%d", &m);
+
+            if (m == 0) break;
+        switch (m) {
+        case 1:
+            printf("데이터 : "); scanf("%d", &x);
+            if (Enque(&que, x) == -1)
+                puts("\a오류 : 디큐에 실패하였습니다.");
+            break;
+
+        case 2:
+            if (Deque(&que, &x) == -1)
+                puts("\a오류 : 디큐에 실패하였습니다.");
+            else
+                printf("디큐한 데이터는 %d입니다.\n", x);
+            break;
+
+        case 3:
+            if (Peek(&que, &x) == -1)
+                puts("\a오류 : 피크에 실패하였습니다.");
+            else
+                printf("피크한 데이터는 %d입니다.\n", x);
+            break;
+
+        case 4:
+            Print(&que);
+            break;
+        }
+    }
+    Terminate(&que);
+    return 0;
+}
